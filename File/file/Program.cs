ed7593596ee567b1ab33763930c70a32f4cd6d09
Yura <yamazaki.kadzuto@gmail.com>
2019-04-20 12:32:52 +0300

    adding new laba's
    adding new laba's

---
+++ File/file/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Дан массив из известных значений
+            double[] numb = new double[22];
+            numb[0] = 0; numb[1] = 0; numb[2] = 0; numb[3] = 0.54; numb[4] = 0.96; numb[5] = 1.36;
+            numb[6] = 1.64; numb[7] = 1.88; numb[8] = 2.02; numb[9] = 2.26; numb[10] = 2.38; numb[11] = 2.5;
+            numb[12] = 2.52; numb[13] = 2.66; numb[14] = 2.72; numb[15] = 2.76; numb[16] = 2.8; numb[17] = 2.82;
+            numb[18] = 2.9; numb[19] = 2.92; numb[20] = 2.94; numb[21] = 2.96;
+
+            // Создание класса StreamWriter
+            StreamWriter w = new StreamWriter("1.txt", false);
+
+            // Запись массива numb в файл
+            foreach (double el in numb) w.WriteLine(el);
+            w.Close();
+
+            // Чтение данных из того же файла
+            StreamReader r = new StreamReader("1.txt");
+
+            // Вывод всех непустых строк в консоль
+            string str;
+            while ((str = r.ReadLine()) != null)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inheritance
+{ 
+    abstract class Tovar
+    {
+        // Дата производства.
+        protected DateTime ManufactureDate;
+ 
+        // Название продукта.
+        protected string Name;
+ 
+        // Цена.
+        protected int Price;
+ 
+        // Срок годности.
+        protected int WorkingLife;
+ 
+        // Вывод информации о товаре.
+        public virtual string Info()
+        {
+            return string.Format("Название продукта - {0}\nЦена - {1}", Name, Price);
+        }
+ 
+        // Соответствие сроку годности.
+        public virtual bool IsTovarToWorkingLife(DateTime currentDate)
+        {
+            return (currentDate < ManufactureDate + new TimeSpan(WorkingLife, 0, 0, 0));
+        }
+    }
+ 
+    // Продукт.
+    class Product : Tovar
+    {
+        // Новый продукт.
+        public Product(string name, int price, DateTime manufactureDate, int workingLife)
+        {
+            Name = name;
+            Price = price;
+            ManufactureDate = manufactureDate;
+            WorkingLife = workingLife;
+        }
+ 
+        public override string Info()
+        {
+            return base.Info() +
+                   string.Format("\nДата производства - {0}\nСрок годности - {1} дней",
+                                 ManufactureDate, WorkingLife);
+        }
+    }
+ 
+    // Партия.
+    class Batch : Tovar
+    {
+        private int count;
+ 
+        // Новая партия.
+        public Batch(string name, int price, int count, DateTime manufactureDate, int workingLife)
+        {
+            this.count = count;
+            Name = name;
+            Price = price;
+            ManufactureDate = manufactureDate;
+            WorkingLife = workingLife;
+        }
+ 
+        public override string Info()
+        {
+            return base.Info() +
+                   string.Format("\nКоличество - {0}\nДата производства - {1}\nСрок годности - {2} дней",
+                                 count, ManufactureDate, WorkingLife);
+        }
+    }
+ 
+    // Комплект.
+    class Set : Tovar
+    {
+        private string list;
+ 
+        // Новый комплект.
+        public Set(string name, int price, string list)
+        {
+            this.list = list;
+            Name = name;
+            Price = price;
+        }
+ 
+        public override string Info()
+        {
+            return base.Info() +
+                   string.Format("\nПеречень продуктов - {0}",
+                                 list);
+        }
+ 
+        // Переопределяем, так как у комплекта бесконечный срок годности.
+        public override bool IsTovarToWorkingLife(DateTime currentDate)
+        {
+            return true;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            List<Tovar> tovari = new List<Tovar>
+            {
+                new Product("хлебушек вечности", 100, Convert.ToDateTime("02.12.2010"), 999999),
+                new Batch("жареные тараканы", 3, 444, Convert.ToDateTime("05.12.2010"), 60),
+                new Set("Шпоры по Высшей Математике", 2, "цветные, белые, прозрачные")
+            };
+
+            // Выводим данные и соответствие сроку годности.
+            DateTime now = DateTime.Now;
+            foreach (Tovar tovar in tovari)
+            {
+                Console.WriteLine(tovar.Info());
+                Console.WriteLine(tovar.IsTovarToWorkingLife(now) ? "Годен" : "Не годен");
+                Console.WriteLine("--------------------------------------------------");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
