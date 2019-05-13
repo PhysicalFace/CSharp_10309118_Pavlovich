3e06028ac703069de4f50c65387319d8556957b5
Yura <yamazaki.kadzuto@gmail.com>
2019-05-13 11:55:10 +0300

    Adding the Interface laba

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    struct WORKER
+    {
+        public string Surname;
+        public string Initials;
+        public string Post;
+        public int Date;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = true;
+            const int N = 3;
+
+            WORKER[] worker = new WORKER[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("Введите информацию о сотруднике №" + (i + 1).ToString() + ":\n");
+                Console.Write("\tФамилия: ");  worker[i].Surname = Console.ReadLine();
+                Console.Write("\tИнициалы: "); worker[i].Initials = Console.ReadLine();
+                Console.Write("\tДолжность: "); worker[i].Post = Console.ReadLine();
+                Console.Write("\tГод поступления на работу: "); worker[i].Date = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("\n--------------------------------------------------\n");
+            }
+
+            Console.Write("Введите стаж работы в организации: ");
+            int experience = Int32.Parse(Console.ReadLine());
+
+            for(int i = 0; i < N; i++)
+            {
+                if ((DateTime.Now.Year - worker[i].Date) > experience)
+                {
+                    Console.WriteLine(worker[i].Surname);
+                    f = false;
+                }
+            }
+
+            if (f)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n\tТаких работников нету!");
+                Console.ResetColor();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- Interface/Interface/Properties/AssemblyInfo.cs
+++ Interface/Interface/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Special_Task")]
+[assembly: AssemblyTitle("Interface")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Special_Task")]
+[assembly: AssemblyProduct("Interface")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("995652aa-bf28-436c-b2b8-310d44117bc8")]
+[assembly: Guid("2c6f9754-5c30-4d1f-b4ca-328c8507be76")]
 
 // Version information for an assembly consists of the following four values:
 //
